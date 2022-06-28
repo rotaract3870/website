0a24d8a8e061a33dec9cbf564301d97ea29fdff0
irayspace <irayspacii@gmail.com>
2022-06-28 15:33:20 +0800

    feat: files and links resources

---
+++ components/elements/resources/resources_area.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import Link from 'next/link'
+
+export default function ResourcesArea() {
+    return (
+        <div className="team_area pt-110 pb-90">
+            <div className="container">
+                <div className="row">
+                    <div className="col-xxl-12">
+                        <div className="section_title mb-50 text-center">
+                            <span className="sub_title sub_title_2">Useful downloadables</span>
+                            <h3 className="title title_2">Files and links</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/layout/header"
 import Breadcrumb from '../components/layout/breadcrumb'
 import Footer2 from '../components/layout/footer2'
+import ResourcesArea from "../components/elements/resources/resources_area"
 
 export default function Resources() {
   return (
@@ -8,6 +9,7 @@ export default function Resources() {
       <Header />
       <main>
         <Breadcrumb title="Resources" backgroundImage="url(/img/bg/breadcrum_bg_2.jpg)" />
+        <ResourcesArea />
         <Footer2 />
       </main>
     </>
