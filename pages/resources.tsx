98192c0d139f9c8f9f01281b9ddbbffd22b891a4
irayspace <irayspacii@gmail.com>
2022-06-28 15:28:05 +0800

    feat: remove club area

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,6 +1,5 @@
 import Header from "../components/layout/header"
 import Breadcrumb from '../components/layout/breadcrumb'
-import ClubArea from '../components/elements/club_directory/club_area'
 import Footer2 from '../components/layout/footer2'
 
 export default function Resources() {
@@ -9,7 +8,6 @@ export default function Resources() {
       <Header />
       <main>
         <Breadcrumb title="Resources" backgroundImage="url(/img/bg/breadcrum_bg_2.jpg)" />
-        <ClubArea />
         <Footer2 />
       </main>
     </>
