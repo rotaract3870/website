0f9271d8e5eaa18fa6342621deaedcc37c803851
irayspace <irayspacii@gmail.com>
2022-06-30 15:23:35 +0800

    feat: added initial resources

--- components/elements/resources/resources_area.tsx
+++ components/elements/resources/resources_area.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-// import Link from 'next/link'
+import Link from 'next/link'
+import resourcesRepo from '../../../data/resources.json';
 
 export default function ResourcesArea() {
     return (
@@ -12,6 +13,17 @@ export default function ResourcesArea() {
                             <h3 className="title title_2">Files and links</h3>
                         </div>
                     </div>
+                </div>
+                <div className="row">
+                    {resourcesRepo.data.map((resource) => {
+                        return (
+                            <div className="col-xxl-3 col-xl-4 col-lg-4 col-sm-6 text-center">
+                                <Link href={resource.link}>
+                                    <a className="g_btn fca_btn1 to_right2 p-40 rad-50">{resource.name}<span></span></a>
+                                </Link>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
