a32f91e1051ecfc140f51435e138f8e8d7f2ff30
irayspace <irayspacii@gmail.com>
2022-07-15 13:57:19 +0800

    feat: by category resources

---
+++ components/elements/resources/resource_link.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+type ResourceLinkProps = {
+    name: string,
+    link: string
+}
+
+export default function ResourceLink({ name, link }: ResourceLinkProps) {
+    return (
+        <div key={name} className="col-xxl-3 col-xl-4 col-lg-4 col-sm-6 text-center">
+            <Link href={link}>
+                <a className="d-block g_btn fca_btn1 to_right2 p-40 rad-50">{name}<span></span></a>
+            </Link>
+        </div>
+    )
+}
--- components/elements/resources/resources_area.tsx
+++ components/elements/resources/resources_area.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
+import ResourceLink from './resource_link';
+
 import resourcesRepo from '../../../data/resources.json';
 
 export default function ResourcesArea() {
@@ -14,17 +16,22 @@ export default function ResourcesArea() {
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    {resourcesRepo.data.map((resource) => {
-                        return (
-                            <div key={resource.name} className="col-xxl-3 col-xl-4 col-lg-4 col-sm-6 text-center">
-                                <Link href={resource.link}>
-                                    <a className="g_btn fca_btn1 to_right2 p-40 rad-50">{resource.name}<span></span></a>
-                                </Link>
+                {resourcesRepo.category_data.map((category) => {
+                    return (
+                        <div className="row mb-80">
+                            <div className="col-xxl-12">
+                                <div className="section_title">
+                                    <h4 className="n_title">{category.name}</h4>
+                                </div>
                             </div>
-                        );
-                    })}
-                </div>
+                            {resourcesRepo.data.filter((resource) => resource.category_id === category.id).map((resource) => {
+                                return (
+                                    <ResourceLink name={resource.name} link={resource.link} />
+                                )
+                            })}
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
