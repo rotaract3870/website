b61f4e4ebdd644b06b330a13ae4ec8df1003d61c
irayspace <irayspacii@gmail.com>
2022-09-04 23:49:59 +0800

    fix(resource_link): mb-25

--- components/elements/resources/resource_link.tsx
+++ components/elements/resources/resource_link.tsx
@@ -8,7 +8,7 @@ type ResourceLinkProps = {
 
 export default function ResourceLink({ name, link }: ResourceLinkProps) {
     return (
-        <div key={name} className="col-xxl-3 col-xl-4 col-lg-4 col-sm-6 text-center">
+        <div key={name} className="col-xxl-3 col-xl-4 col-lg-4 col-sm-6 text-center mb-25">
             <Link href={link}>
                 <a className="d-block g_btn fca_btn1 to_right2 p-40 rad-50">{name}<span></span></a>
             </Link>
